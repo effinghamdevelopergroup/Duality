e73874dcd7a38f22690fa54f456410bb06901cb3
Joey Meyer <joeymeyer1986@gmail.com>
2022-01-23 15:31:33 -0600

    simple movement

--- scripts/player/_Player.gd
+++ scripts/player/_Player.gd
@@ -1,29 +1,21 @@
 extends KinematicBody
 
-onready var head = $Head
-onready var camera = $Camera
+var gravity = -9.8
+var velocity = Vector3()
+var camera
 
-var max_speed = 400
-var speed = 0
-var acceleration = 1200
-var move_direction
-var moving = false
-var destination = Vector3()
-var movement = Vector3()
+const SPEED = 6
+const ACCELERATION = 3
+const DE_ACCELERATION = 5
+
+func _ready():
+	camera = get_node("Camera").get_global_transform()
 
 func _input(event):
 	pass
 
 func _unhandled_input(event):
-	if event is InputEventKey and event.pressed:
-		if Input.is_action_just_pressed("Forward"):
-			print("Moving Forward")
-		if Input.is_action_just_pressed("Backward"):
-			print("Moving Backward")
-		if Input.is_action_just_pressed("Left"):
-			print("Moving Left")
-		if Input.is_action_just_pressed("Right"):
-			print("Moving Right")
+	pass
 
 func _process(delta):
 	AnimationLoop()
@@ -32,7 +24,35 @@ func _physics_process(delta):
 	MovementLoop(delta)
 	
 func MovementLoop(delta):
-	pass
+	var direction = Vector3()
+
+	if Input.is_action_pressed("Backward"):
+		direction += camera.basis[2]
+	if Input.is_action_pressed("Forward"):
+		direction -= camera.basis[2]
+	if Input.is_action_pressed("Right"):
+		direction += camera.basis[0]
+	if Input.is_action_pressed("Left"):
+		direction -= camera.basis[0]
+
+	direction.y = 0
+	direction = direction.normalized()
+
+	velocity.y += delta * gravity
+
+	var hv = velocity
+	hv.y = 0
+
+	var new_pos = direction * SPEED
+	var accel = DE_ACCELERATION
+
+	if (direction.dot(hv) > 0):
+		accel = ACCELERATION
+	
+	hv = hv.linear_interpolate(new_pos, accel * delta)
+	velocity.x = hv.x
+	velocity.z = hv.z
+	velocity = move_and_slide(velocity, Vector3(0,1,0))
 	
 func AnimationLoop():
 	pass
