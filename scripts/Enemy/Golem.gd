0a0b2a9fd2abb18acafe1b3fa46bbd53ff83e9f4
happyzach <schneider61938@gmail.com>
2022-01-28 20:08:50 -0600

    check for results

--- scripts/Enemy/Golem.gd
+++ scripts/Enemy/Golem.gd
@@ -1,9 +1,12 @@
 extends KinematicBody
 
+export var jump = -350
 export var speed = 100
+export var gravity = 70
 var target
 var space_state
-
+var velocity = Vector3.ZERO
+onready var animation = $GolemModel/AnimationPlayer
 
 
 func _ready():
@@ -11,12 +14,17 @@ func _ready():
 
 
 func _physics_process(delta):
+	apply_gravity(delta)
 	if target:
 		var result = space_state.intersect_ray(global_transform.origin, target.global_transform.origin)
-		print(result)
-		if result.collider.is_in_group("Player"):
-			look_at(target.global_transform.origin, Vector3.UP)
-			move_to_target(delta)
+		if result:
+			if result.collider.is_in_group("Player"):
+				look_at(target.global_transform.origin, Vector3.UP)
+				move_to_target(delta)
+
+
+func apply_gravity(delta):
+	velocity.y -= gravity * delta
 
 
 func _on_Area_body_entered(body):
@@ -31,5 +39,4 @@ func _on_Area_body_exited(body):
 
 func move_to_target(delta):
 	var direction = (target.transform.origin - transform.origin).normalized()
-	print("HI")
 	move_and_slide(direction * speed * delta, Vector3.UP)
