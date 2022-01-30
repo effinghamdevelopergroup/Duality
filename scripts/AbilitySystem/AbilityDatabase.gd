56bfa2532211af2c82d9d626e6b4f4b2e4a4e03c
Joey Meyer <joeymeyer1986@gmail.com>
2022-01-29 23:02:11 -0600

    Dialog progress

--- scripts/AbilitySystem/AbilityDatabase.gd
+++ scripts/AbilitySystem/AbilityDatabase.gd
@@ -2,3 +2,22 @@ extends Node
 
 export (PackedScene) var Fireball = preload("res://scenes/Game/Abilities/Fireball.tscn")
 export (PackedScene) var IceLance = preload("res://scenes/Game/Abilities/IceLance.tscn")
+
+var Plyr setget SetPlayer
+
+var Choice = "NotSet" setget SetAbility
+
+signal ability_learned
+
+func SetAbility(objPassed):
+	Choice = objPassed
+	if Choice == "Fireball":
+		Plyr.known_abilities["Fireball"] = Fireball
+		Plyr.active_ability = Plyr.known_abilities["Fireball"]
+	else:
+		Plyr.known_abilities["IceLance"] = IceLance
+		Plyr.active_ability = Plyr.known_abilities["IceLance"]
+
+func SetPlayer(objPassed):
+	Plyr = objPassed
+	print(objPassed.known_abilities)
--- scripts/Dialog/Dialog.gd
+++ scripts/Dialog/Dialog.gd
@@ -3,6 +3,7 @@ extends Node
 onready var text_area = $RichTextLabel
 var dialog = {"FireOrIce":["Old Man", "Fire... or... Ice?",["Fire", "Ice"]]}
 var page = 0
+signal learn_ability
 
 func _ready():
 	var speaker_name_length = dialog["FireOrIce"][0].length()
@@ -15,13 +16,17 @@ func _ready():
 		button.connect("pressed", self, value)
 		button.margin_left = 15
 		$ButtonArea.add_child(button)
-		pass
+
+func learn_ability(choice):
+	AbilityDatabase.Choice = choice
 
 func Fire():
 	print("Picked Fire")
+	learn_ability("Fireball")
 	
 func Ice():
 	print("Picked Ice")
+	learn_ability("Icelance")
 
 func _on_Timer_timeout():
 	text_area.set_visible_characters(text_area.get_visible_characters() + 1)
