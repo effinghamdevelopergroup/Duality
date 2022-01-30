5dc71fdea2248c49f8d0a7c9ce297027de208f64
Joey Meyer <joeymeyer1986@gmail.com>
2022-01-30 12:36:51 -0600

    credit start

---
+++ scripts/Credits/Credits.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var Credits = {
+	"Developers":[
+		"Zach",
+		"Logan Malcome",
+		"Bradley Kilgore",
+		"William Sealge", 
+		"Alexander Harms", 
+		"Joey Meyer"
+	],
+	"Sound":["Zach Schneider"],
+	"Music":["Zach Schneider"],
+	"Art":["Logan Malcome"],
+	"Story":["Schuyler Curtis"]
+}
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	pass
